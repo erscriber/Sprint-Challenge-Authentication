f7a4021e21b9154af4c8490bffc8e934b32eb47e
erscriber <erscriber@gmail.com>
2019-04-05 21:43:28 -0400

    Register endpoint complete and tested with Postman

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,8 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+const Users = require('../users/user-model.js');
 const { authenticate } = require('../auth/authenticate');
 
 module.exports = server => {
@@ -10,10 +13,27 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users
+    .add(user)
+    .then(saved => {
+      res
+        .status(201)
+        .json({ message: "Registration for successful!" });
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "Registration cannot be completed. "});
+    })
 }
 
 function login(req, res) {
   // implement user login
+  
 }
 
 function getJokes(req, res) {
